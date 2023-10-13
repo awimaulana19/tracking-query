e030ae2d3f4836eda9cb83080212252581d5ca79
awimaulana19 <awimaulana19@gmail.com>
2023-10-13 10:10:35 +0800

    first commit

---
+++ public/sql/logfile1.sql
@@ -0,0 +1,15 @@
+
+#
+# TRACKING QUERY
+#
+
+UPDATE `00_`.`md_wilayah_administrasi` SET k1 = '21' WHERE k1 = '32';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.89.22.2001' WHERE kode = '32.89.22.2001';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.89.22.2002' WHERE kode = '32.89.22.2002';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.89.22.2003' WHERE kode = '32.89.22.2003';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.89.22.2004' WHERE kode = '32.89.22.2004';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.89.22.2005' WHERE kode = '32.89.22.2005';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.89.22.2006' WHERE kode = '32.89.22.2006';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.89.22.2007' WHERE kode = '32.89.22.2007';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.89.22.2008' WHERE kode = '32.89.22.2008';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.89.22.2009' WHERE kode = '32.89.22.2009';
---
+++ public/sql/logfile2.sql
@@ -0,0 +1,43 @@
+
+#
+# TRACKING QUERY
+#
+
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.25.2006', k1 = '89' WHERE kode = '92.09.25.2006';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.25.2007', k1 = '89' WHERE kode = '92.09.25.2007';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.25.2008', k1 = '89' WHERE kode = '92.09.25.2008';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.25.2009', k1 = '89' WHERE kode = '92.09.25.2009';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.25.2010', k1 = '89' WHERE kode = '92.09.25.2010';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.26', k1 = '89' WHERE kode = '92.09.26';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.26.2001', k1 = '89' WHERE kode = '92.09.26.2001';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.26.2002', k1 = '89' WHERE kode = '92.09.26.2002';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.26.2003', k1 = '89' WHERE kode = '92.09.26.2003';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.26.2004', k1 = '89' WHERE kode = '92.09.26.2004';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.09.26.2005', k1 = '89' WHERE kode = '92.09.26.2005';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '98.09.05.2002', k1 = '98' WHERE kode = '81.09.05.2002';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '98.09.05.2003', k1 = '98' WHERE kode = '81.09.05.2003';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '98.09.05.2004', k1 = '98' WHERE kode = '81.09.05.2004';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '98.09.05.2005', k1 = '98' WHERE kode = '81.09.05.2005';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '98.09.05.2006', k1 = '98' WHERE kode = '81.09.05.2006';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '98.09.05.2007', k1 = '98' WHERE kode = '81.09.05.2007';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.25.2006', k2 = '77' WHERE kode = '89.77.25.2006';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.25.2007', k2 = '77' WHERE kode = '89.77.25.2007';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.25.2008', k2 = '77' WHERE kode = '89.77.25.2008';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.25.2009', k2 = '77' WHERE kode = '89.77.25.2009';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.25.2010', k2 = '77' WHERE kode = '89.77.25.2010';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.26', k2 = '77' WHERE kode = '89.77.26';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.26.2001', k2 = '77' WHERE kode = '89.77.26.2001';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.26.2002', k2 = '77' WHERE kode = '89.77.26.2002';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.26.2003', k2 = '77' WHERE kode = '89.77.26.2003';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.26.2004', k2 = '77' WHERE kode = '89.77.26.2004';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '89.77.26.2005', k2 = '77' WHERE kode = '89.77.26.2005';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.01.97.2002', k3 = '97' WHERE kode = '21.01.97.2002';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.01.97.2003', k3 = '97' WHERE kode = '21.01.97.2003';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.01.97.2004', k3 = '97' WHERE kode = '21.01.97.2004';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.01.97.2005', k3 = '97' WHERE kode = '21.01.97.2005';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '36.92.08.2002', k2 = '92' WHERE kode = '36.92.08.2002';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '36.92.08.2005', k2 = '92' WHERE kode = '36.92.08.2005';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '36.92.08.2006', k2 = '92' WHERE kode = '36.92.08.2006';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '36.92.08.2007', k2 = '92' WHERE kode = '36.92.08.2007';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '36.92.08.2008', k2 = '92' WHERE kode = '36.92.08.2008';
+UPDATE `00_`.`md_wilayah_administrasi` SET kode = '21.01.97.2067', k4 = '2067' WHERE kode = '21.01.97.2002';
